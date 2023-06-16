f6fccda120f8271ae4c107279da2fb31ee69e60f
Pavlo <pavelgomail@gmail.com>
2023-06-16 22:21:54 +0100

    2 step

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -12,6 +12,11 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], -1), [5])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5], -10), [1, 2, 3, 4, 5])
+
+
 
 
 if __name__ == '__main__':
